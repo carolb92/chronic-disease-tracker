00c0c87170357ed835d796ffb915b17f16032db1
Carol B <carolb@bu.edu>
2026-06-19 15:13:06 -0400

    Add SNOMED and LOINC code constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,33 @@
+export const TRACKED_CONDITION_SNOMED_CODES = [
+	"46635009", //T1DM
+	"44054006", //T2DM
+	"38341003", // general HTN (hypertensive disorder)
+	"59621000", // essential HTN
+	"55822004", // general hyperlipidemia
+	"13644009", // hypercholesterolemia (disorder)
+	"398036000", // familial hypercholesterolemia
+	"238079002", // Familial hypercholesterolemia - heterozygous
+	"238078005", // Familial hypercholesterolemia - homozygous
+	"403829002", // Familial hypercholesterolemia due to heterozygous low density lipoprotein receptor mutation
+	"403831006", // Familial hypercholesterolemia due to genetic defect of apolipoprotein B
+];
+
+export const DIABETES_LOINC_CODES = [
+	"1558-6", // Fasting glucose
+	"4548-4", // Hemoglobin A1c
+	"29463-7", // Body weight
+];
+
+export const HTN_LOINC_CODES = [
+	"8480-6", // systolic BP
+	"8462-4", // diastolic BP
+	"85354-9", // Blood pressure panel (systolic and diastolic, combined panel code)
+	"8478-0", // mean BP
+];
+
+export const HLD_LOINC_CODES = [
+	"2089-1", // LDL
+	"2093-3", // total cholesterol
+	"2085-9", // HDL
+	"2571-8", // triglycerides
+];

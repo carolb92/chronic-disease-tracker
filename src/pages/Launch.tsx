d6cdccc273a6accff84483c878e71dda71a7171e
Carol B <carolb@bu.edu>
2026-06-19 15:12:35 -0400

    Launch route oauth config

--- src/pages/Launch.tsx
+++ src/pages/Launch.tsx
@@ -1,3 +1,13 @@
+import { useEffect } from "react";
+import FHIR from "fhirclient";
+
 export default function Launch() {
-	return <div>Launch</div>;
+	useEffect(() => {
+		FHIR.oauth2.authorize({
+			clientId: "chronic-disease-tracker",
+			scope: "launch openid fhirUser patient/*.read",
+			redirectUri: "/app",
+		});
+	}, []);
+	return null;
 }
